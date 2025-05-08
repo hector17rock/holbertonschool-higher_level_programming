b191e0d315e15a9b90b7e3cf67c49d4a0fd18731
Hector Soto <hector_17rock@hotmail.com>
2025-05-08 13:50:03 -0400

    update

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-def islower(c):
-    """Check if a character is a lowercase letter."""
-    return ord(c) >= 97 and ord(c) <= 122
+print("a is {}".format("lower" if "a".islower() else "upper"))
