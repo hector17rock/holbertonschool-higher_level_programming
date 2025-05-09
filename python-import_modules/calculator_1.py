daf6d89deaed31cb6931351476d60308130907fb
Hector Soto <hector_17rock@hotmail.com>
2025-05-09 11:12:14 -0400

    feat: add import

---
+++ python-import_modules/calculator_1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Return the addition of a and b."""
+    return a + b
+
+def sub(a, b):
+    """Return the subtraction of b from a."""
+    return a - b
+
+def mul(a, b):
+    """Return the multiplication of a and b."""
+    return a * b
+
+def div(a, b):
+    """Return the division of a by b."""
+    return a / b
+
