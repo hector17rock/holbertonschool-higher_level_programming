e3806d9db42234320ec88923bb851137caaec3c0
Hector Soto <hector_17rock@hotmail.com>
2025-05-13 14:00:00 -0400

    update

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@ def safe_print_list(my_list=[], x=0):
     """Prints x elements of a list safely.
 
     Args:
-        my_list (list): The list to print elements from.
+        my_list (list): The list to print from.
         x (int): Number of elements to attempt to print.
 
-        Returns:
-            int: The actual number of elements printed.
-        """
+    Returns:
+        int: Actual number of elements printed.
+    """
     count = 0
     try:
         for i in range(x):
