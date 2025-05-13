289ad7e117e872cbb8bb38b8885a63840c8411cb
Hector Soto <hector_17rock@hotmail.com>
2025-05-13 14:15:13 -0400

    update

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_interger(value):
-    """Prints an interger using '{;d}'.format
+    """Prints an interger using '{:d}'.format
 
     Args:
         Value: Any type of value.
 
     Returns:
-        Trus if value is an interger and printed, False otherwise.
+        True if value is an interger and printed, False otherwise.
     """
     try:
         print("{:d}".format(value))
