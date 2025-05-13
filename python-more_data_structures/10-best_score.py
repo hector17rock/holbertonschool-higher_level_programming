a7fb933076874026e317f0e2f8aa9fb959825aa5
Hector Soto <hector_17rock@hotmail.com>
2025-05-13 12:31:35 -0400

    Update 0-square_matrix_simple.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    return max(a_dictionary, key=a_dictionary.get)
+def multiply_list-map(my_list=[], number=0):
+    """Return a new list with all values multiplied by the given number."""
+    return list(map(lambda x: x * number, my_list))
