0078eaee2b8e1d569ab5efe4b3550d05fe0cade5
Hector Soto <hector_17rock@hotmail.com>
2025-05-13 12:32:56 -0400

    Update 0-square_matrix_simple.py

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    ""Return a new list with all values multiplied by the given number."""
-return list(map(lambda x: x * number, my_list))
+    """Return a new list with all values multiplied by the given number."""
+    return list(map(lambda x: x * number, my_list))
