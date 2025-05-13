15f27bd2f5b3734e98440ccc84461c1ff18d0a83
Hector Soto <hector_17rock@hotmail.com>
2025-05-13 10:53:43 -0400

    update

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def print_reversed_listinterger(my_list=[]):
-    """Prints all the intergers of a list in reverse order"""
+def print_reversed_list_interger(my_list=[]):
+    """Prints all intergers of a list in reverse order"""
     if my_list:
         for i in reversed(my_list):
             print("{:d}".format(i))
