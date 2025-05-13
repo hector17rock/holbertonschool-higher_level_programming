cc308c8e4ca9d2b6d1b4273d061d3bcd2fbc9748
Hector Soto <hector_17rock@hotmail.com>
2025-05-13 10:45:12 -0400

    update

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -8,12 +8,10 @@ def element_at(my_list, idx):
 
 if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5]
-    
     # Test with valid indices
     print("Element at index 0: {}".format(element_at(my_list, 0)))
     print("Element at index 2: {}".format(element_at(my_list, 2)))
     print("Element at index 4: {}".format(element_at(my_list, 4)))
-    
     # Test with invalid indices
     print("Element at index -1: {}".format(element_at(my_list, -1)))
     print("Element at index 5: {}".format(element_at(my_list, 5)))
