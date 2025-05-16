6a9a173e24832e876bd991d73b53eefd09d69766
Hector Soto <hector_17rock@hotmail.com>
2025-05-16 14:38:14 -0400

    update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""This module provides a function that divides
+all the elements of a matrix."""
+
+
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by a given number.
 
