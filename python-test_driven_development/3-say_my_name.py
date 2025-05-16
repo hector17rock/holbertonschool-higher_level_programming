4b0951c7d63c121a9d16e85d2f781f5685b09b06
Hector Soto <hector_17rock@hotmail.com>
2025-05-16 14:58:47 -0400

    update

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,11 +3,11 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints My name is <first name> <last name>.
+    """Prints 'My name is <first name> <last name>'.
 
     Args:
         first_name (str): The first name.
-        last_name (str): The last name (optional).
+        last_name (str): The last name. Default is an empty string.
 
     Raises:
         TypeError: If first_name or last_name is not a string.
@@ -17,8 +17,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    # Avoid extra space if last_name is empty
-    if not last_name:
-        print("My name is {}".format(first_name))
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
