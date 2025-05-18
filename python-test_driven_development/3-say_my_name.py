5140eb0715997d7b44e4f2660558206188a5997d
Hector Soto <hector_17rock@hotmail.com>
2025-05-18 16:41:58 -0400

    update

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -18,6 +18,8 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("last_name must be a string")
 
     if last_name:
-        print("My name is {} {}".format(first_name, last_name))
+        print("My name is {} {}".format(
+            first_name, last_name))
     else:
-        print("My name is {}".format(first_name))
+        print("My name is {} ".format(
+            first_name))
