2185da85a0f4978e8eee32ba22ebccf948887ec8
Hector Soto <hector_17rock@hotmail.com>
2025-05-18 17:11:24 -0400

    update

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module provides a function to print indented text.
+"""
+
+
+def text_indentation(text):
+    """
+    Prints text with 2 newlines after '.', '?', and ':' characters.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
