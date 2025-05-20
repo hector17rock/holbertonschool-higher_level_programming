7a3d300943c78e6f568a9db70ed8101bba756277
Hector Soto <hector_17rock@hotmail.com>
2025-05-20 11:48:16 -0400

    update

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Represents a class Square with a private instance attribute."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square
+    Args:
+        size (init): the size of the square.
+    """
+    self.__size = Size
