98d56764a4950bbc7465a85a1c4c0a9e327f5a66
Hector Soto <hector_17rock@hotmail.com>
2025-05-20 12:11:32 -0400

    update

---
+++ python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a class Square with validation."""
+
+
+class Square:
+    """Represents a square."""
+
+    def__init__(self, size=0):
+        """Initializes a square.
+
+        Args:
+            size (int): The size of the square (default is 0).
+
+        Raises:
+            TypeError: If size is not an interger.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an interger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
