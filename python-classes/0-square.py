51964bab0d8d73e5316f95fbaea5f9a3ed086caa
Hector Soto <hector_17rock@hotmail.com>
2025-05-20 11:40:33 -0400

    update

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines an empty class Square."""
+
+
+class Square:
+    """An empty Square class."""
+    pass
+
+
+class Square:
+    """Defines a square by sizes."""
+    def __init__(self, size):
+        self.size = size
