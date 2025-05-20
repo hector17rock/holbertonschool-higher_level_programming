17d0e7236b5e161b88ae7d1061e695691db386f0
Hector Soto <hector_17rock@hotmail.com>
2025-05-20 12:13:30 -0400

    update

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,18 +5,18 @@
 class Square:
     """Represents a square."""
 
-    def__init__(self, size=0):
+    def __init__(self, size=0):
         """Initializes a square.
 
         Args:
             size (int): The size of the square (default is 0).
 
         Raises:
-            TypeError: If size is not an interger.
+            TypeError: If size is not an integer.
             ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an interger")
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
