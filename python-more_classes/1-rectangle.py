f42c22630d7b8302cb86e5725354be50ce9e7329
Hector Soto <hector_17rock@hotmail.com>
2025-05-22 11:14:02 -0400

    update

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class with width and height."""
+
+
+class Rectangle:
+    """Represents a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
