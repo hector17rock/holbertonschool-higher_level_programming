033b15799ddfc7f5d2ad8e423fcb2197ec479bca
Hector Soto <hector_17rock@hotmail.com>
2025-06-03 13:32:30 -0400

    update

---
+++ python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a Student class with JSON serialization."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance.
+
+        Args:
+            first_name (str): The student's first name.
+            last_name (str): The student's last name.
+            age (int): The student's age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns the dictionary representation of a Student instance."""
+        return self.__dict__
