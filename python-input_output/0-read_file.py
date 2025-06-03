1cd90c4fb0f7775b39d5d8e25df73aad10c7a734
Hector Soto <hector_17rock@hotmail.com>
2025-06-03 13:16:47 -0400

    update

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Defines a function that reads a text file and prints it to stdout."""
 
+
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout.
-    
+
     Args:
         filename (str): The name of the file to read.
     """
