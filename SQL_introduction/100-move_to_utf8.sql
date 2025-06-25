92ad0261501097a67150b1affd8180db5b35cd66
Hector Soto <hector_17rock@hotmail.com>
2025-06-25 15:32:15 -0400

    TASK100 CORRECTION

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,12 +1,4 @@
 -- Converts hbtn_0c_0 database to UTF8 (utf8mb4, collate utf8mb4_unicode_ci)
--- Converts database hbtn_0c_0
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Use the database
 USE hbtn_0c_0;
-
--- Converts table first_table
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Converts field name in first_table
-ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
