921fa3ef0c43f20d34453b3db681a817e0594e29
Hector Soto <hector_17rock@hotmail.com>
2025-07-17 12:57:11 -0400

    TASK03

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,8 @@
+document.querySelector('#toogle_header').addEventListener('click', function () {
+  const header = document.querySelector('header')) {
+  fi (header.classList.contains('red')) {
+    header.classList.replace('red', 'green');
+  } else {
+    header.classList.replace('green', 'red');
+  }
+});
