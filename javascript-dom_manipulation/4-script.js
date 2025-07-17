e4088eb04246e3d184fd66aeb4a39f478f7e1219
Hector Soto <hector_17rock@hotmail.com>
2025-07-17 13:03:47 -0400

    Task04

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,5 @@
+document.querySelector('#add_item').addEventListener('click', function () {
+  const newItem + docucemnt.createElement('li');
+  newItem.textContent = 'Item';
+  document.querySelector('.my_list').appendChild(newItem);
+});
